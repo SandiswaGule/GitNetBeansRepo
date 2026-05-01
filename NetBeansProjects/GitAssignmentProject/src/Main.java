5f67cae8fc109452e2dc52108d2f7c4f87cdfef5
Sandiswa <sosagule135790@gmail.com>
2026-05-01 12:12:15 +0200

    Edit Main.java on conflict-demo branch

--- NetBeansProjects/GitAssignmentProject/src/Main.java
+++ NetBeansProjects/GitAssignmentProject/src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Netbeans. Welcome version control using Git - MAIN BRANCH EDIT");
+        System.out.println("Hello Netbeans. Welcome version control using Git - CONFLICT DEMO EDIT");
     }
 }
